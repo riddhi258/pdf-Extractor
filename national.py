86bd5379b478b6c4cc7205324cef8a51b2247340
riddhi258 <upadhyayriddhi351@gmail.com>
2026-07-20 21:17:11 +0530

    Refactor regex for vehicle info extraction

--- national.py
+++ national.py
@@ -353,10 +353,7 @@ def extract_policy_details(text, file_name, allow_underscore_names):
     # --- Fuel Type ---
     fuel = find(r"(?:Type\s*of\s*Fuel|Fuel\s*Type)\s*[:\-]?\s*([A-Za-z]+)", t)
         # --- Vehicle Info ---
-    vehicle_info = find(
-        r"(?:Make|Manufacturer)\s*:\s*([A-Za-z0-9&.\- ]{2,50})",
-        t
-    )
+   vehicle_info = find(r"(?:Make|Manufacturer)\s*[:\-]?\s*([A-Za-z0-9\s&\.\-]+)", t)
 
    # --- Registration Number ---
     reg_no = find(
